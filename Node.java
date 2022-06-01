eaf8fb86c22c4eb543d8538c8dc2e1b4ef04df62
Nilesh <nlakade29@gmail.com>
2022-06-01 21:28:52 +0530

    last commit

---
+++ Node.java
@@ -0,0 +1,12 @@
+package link_list;
+public class Node {
+	int coeff;
+   int pow;
+   Node next;
+   
+   public Node(int a, int b ) {
+   	coeff = a;
+   	pow = b;
+   	next = null;
+   }
+}
---
+++ Polynomial.java
@@ -0,0 +1,28 @@
+package link_list;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Polynomial {
+	public Node addPolynomial(Node p1, Node p2) {
+		Node a = p1, b = p2, newHead = new Node(0, 0), c = newHead;
+		while (a != null || b != null) {
+			if (a == null) {
+				c.next = b;
+				break;
+			} else if (b == null) {
+				c.next = a;
+				break;
+			} else if (a.pow == b.pow) {
+				c.next = new Node(a.coeff + b.coeff, a.pow);
+				a = a.next;
+				b = b.next;
+			} else if (a.pow < b.pow) {
+				c.next = new Node(b.coeff, b.pow);
+				b = b.next;
+			}
+			c = c.next;
+		}
+		return newHead.next;
+	}
+}
